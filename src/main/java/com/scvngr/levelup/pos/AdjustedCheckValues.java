22d65ae502c57c689c270b8d06641326dda343f9
Navid Matin <navid@thelevelup.com>
2017-05-09 10:22:23 -0400

    Changed the project to maven project, I still have no idea what I am doing!

---
+++ src/main/java/com/scvngr/levelup/pos/AdjustedCheckValues.java
@@ -0,0 +1,36 @@
+package com.scvngr.levelup.pos;
+
+public class AdjustedCheckValues {
+    private int _spendAmount;
+    private int _taxAmount;
+    private int _excemptionsAmount;
+
+    public AdjustedCheckValues(int spendAmount, int taxAmount, int exemptionAmount)
+    {
+        _spendAmount = spendAmount;
+        _taxAmount = taxAmount;
+        _excemptionsAmount = exemptionAmount;
+    }
+
+    public int getSpendAmount() {
+        return _spendAmount;
+    }
+
+    public void setSpendAmount(int _spendAmount) {
+        this._spendAmount = _spendAmount;
+    }
+
+    public int getTaxAmount() {
+        return _taxAmount;
+    }
+
+    public int getExcemptionsAmount() {
+        return _excemptionsAmount;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("SpendAmount=%s;TaxAmount=%s;ExemptionAmount=%s", _spendAmount, _taxAmount, _excemptionsAmount);
+    }
+}
